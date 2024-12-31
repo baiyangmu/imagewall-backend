45bf41fefc60296de1232701fb90ee5569fe9f84
baiyangmu <baiyangmu333@gmail.com>
2024-12-31 20:26:57 +0800

    update config

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 import os
 
 class Config:
-    MYSQL_HOST = 'localhost'  # MySQL 主机地址
-    MYSQL_USER = 'root'       # MySQL 用户名
-    MYSQL_PASSWORD = 'root'  # MySQL 密码
+    MYSQL_HOST = '172.26.94.166'  # MySQL 主机地址
+    MYSQL_USER = 'imagewall'       # MySQL 用户名
+    MYSQL_PASSWORD = 'ysfsbym22A123456a,./'  # MySQL 密码
     MYSQL_DB = 'imagewall'    # 数据库名称
     MYSQL_CURSORCLASS = 'DictCursor'  # 返回字典类型的查询结果
     UPLOAD_FOLDER = './static/images'  # 图片保存的文件夹
